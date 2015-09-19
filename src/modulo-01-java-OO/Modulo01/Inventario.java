07d4e0f408cebf4f00b071851dc4a850e6b56fd0
Ester Vincensi <ester.ines@gmail.com>
2015-09-19 10:39:20 -0300

    Exercicios 3 e 4 - get ItemComMaiorQuantidade e OrdenarItens com alguns testes

--- src/modulo-01-java-OO/Modulo01/Inventario.java
+++ src/modulo-01-java-OO/Modulo01/Inventario.java
@@ -25,4 +25,25 @@ public class Inventario
         }
         return descricao;
     }
+
+    public Item getItemComMaiorQuantidade(){
+        int maior = 0;
+        for(int i=0; i<itens.size();i++){
+            if(itens.get(i).getQuantidade()>itens.get(maior).getQuantidade()){
+                maior=i;
+            }
+        }
+        return this.itens.get(maior);
+    }
+
+    public void ordenarItens(){
+        Item aux;
+        for(int i=0; i<itens.size()-1; i++){
+            if(itens.get(i).getQuantidade()>itens.get(i+1).getQuantidade()){
+                aux = itens.get(i);
+                itens.set(i, itens.get(i+1));
+                itens.set(i+1, aux);
+            }
+        }
+    }
 }
--- src/modulo-01-java-OO/Modulo01/InventarioTest.java
+++ src/modulo-01-java-OO/Modulo01/InventarioTest.java
@@ -1,5 +1,4 @@
 
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -13,38 +12,72 @@ public class InventarioTest
         i.ganharItens(new Item("Arma",1));
         assertEquals(new Item("Arma",1),i.getItens().get(0));
     }
-    
+
     @Test
     public void Adicionar30ArmasAoInventario(){
         Inventario i = new Inventario();
         i.ganharItens(new Item("Arma",30));
         assertEquals(new Item("Arma",30),i.getItens().get(0));
-        
+
     }
-    
+
     @Test
     public void RemoverUmaArmaDoInventario(){
         Inventario i = new Inventario();
         Item arma = new Item("Arma",1);        
         i.ganharItens(arma);
         i.ganharItens(arma);
-        
+
         i.removerItens(arma);
-        
-        
+
         assertEquals(new Item("Arma",1),i.getItens().get(0));
     }
-    
+
     @Test
     public void recebeDescricoesDeFlechaArmaEPocao(){
         Inventario i = new Inventario();
         i.ganharItens(new Item("Flecha",1));
         i.ganharItens(new Item("Arma",1));
         i.ganharItens(new Item("Pocao",1));
-        
+
         String resposta = i.getDescricoesItens();
-        
+
         assertEquals("Flecha,Arma,Pocao",resposta);
     }
-    
+
+    @Test
+    public void getItemComMaiorQuantidadeRetornaItemComMaiorQuantidade(){
+        Inventario i = new Inventario();
+        i.ganharItens(new Item("Flecha",55));
+        i.ganharItens(new Item("Arma",79));
+        i.ganharItens(new Item("Pocao",10));
+
+        assertEquals(new Item("Arma",79),i.getItemComMaiorQuantidade());
+
+    }
+
+    @Test
+    public void getItemComMaiorQuantidadeRetornaPrimeiroInseridoSeTemDuasQuantidadesIguais(){
+        Inventario i = new Inventario();
+        i.ganharItens(new Item("Flecha",55));
+        i.ganharItens(new Item("Arma",10));
+        i.ganharItens(new Item("Pocao",55));
+
+        assertEquals(new Item("Flecha",55),i.getItemComMaiorQuantidade());
+    }
+
+    @Test
+    public void Inserindo3ItensElesSaoOrdenadosEmFormaAscendenteOrdenarItens(){
+        Inventario i = new Inventario();
+        i.ganharItens(new Item("Flecha",55));
+        i.ganharItens(new Item("Arma",10));
+        i.ganharItens(new Item("Pocao",9000));
+        
+        i.ordenarItens();
+        
+        assertEquals(new Item("Arma",10),i.getItens().get(0));
+        assertEquals(new Item("Flecha",55),i.getItens().get(1));    
+        assertEquals(new Item("Pocao",9000),i.getItens().get(2));
+        
+    }
 }
