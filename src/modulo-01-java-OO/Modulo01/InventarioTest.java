c9e45ca2bec13dec5dbaa31977ce5ab7ddbc9cbb
Ester Vincensi <ester.ines@gmail.com>
2015-09-19 16:09:11 -0300

    Adicionados cenários de testes para ordenarItens()

--- src/modulo-01-java-OO/Modulo01/InventarioTest.java
+++ src/modulo-01-java-OO/Modulo01/InventarioTest.java
@@ -110,4 +110,33 @@ public class InventarioTest
         assertEquals(new Item("Flecha",307),i.getItens().get(9));
 
     }
+    
+    @Test
+    public void OrdenarItensComQuantidadeNegativa(){
+        Inventario i = new Inventario();
+
+        i.ganharItens(new Item("Flecha",-55));
+        i.ganharItens(new Item("Flecha",10));
+        i.ganharItens(new Item("Flecha",20));
+        i.ganharItens(new Item("Flecha",-32));
+        i.ganharItens(new Item("Flecha",64));
+        i.ganharItens(new Item("Flecha",97));
+        i.ganharItens(new Item("Flecha",-12));
+        i.ganharItens(new Item("Flecha",-3));
+        i.ganharItens(new Item("Flecha",11));
+        i.ganharItens(new Item("Flecha",9));
+
+        i.ordenarItens();
+
+        assertEquals(new Item("Flecha",-55),i.getItens().get(0));
+        assertEquals(new Item("Flecha",-32),i.getItens().get(1));
+        assertEquals(new Item("Flecha",-12),i.getItens().get(2));
+        assertEquals(new Item("Flecha",-3),i.getItens().get(3));
+        assertEquals(new Item("Flecha",9),i.getItens().get(4));
+        assertEquals(new Item("Flecha",10),i.getItens().get(5));
+        assertEquals(new Item("Flecha",11),i.getItens().get(6));
+        assertEquals(new Item("Flecha",20),i.getItens().get(7));
+        assertEquals(new Item("Flecha",64),i.getItens().get(8));
+        assertEquals(new Item("Flecha",97),i.getItens().get(9));
+    }
 }
