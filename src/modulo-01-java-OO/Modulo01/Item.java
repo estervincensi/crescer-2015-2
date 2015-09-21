394adfe761c362c79aa9c02cd8100f27eb3d5bfb
Ester Vincensi <ester.ines@gmail.com>
2015-09-21 17:01:32 -0300

    Exercicio Refatoração

--- src/modulo-01-java-OO/Modulo01/Item.java
+++ src/modulo-01-java-OO/Modulo01/Item.java
@@ -8,27 +8,32 @@ public class Item
         this.descricao = descricao;
         this.quantidade = quantidade;
     }
-    
+
     public String getDescricao(){
         return this.descricao;
     }
-    
+
     public int getQuantidade(){
         return this.quantidade;
     }
-    
+
     public void aumenta1000Unidades(){
         quantidade+=1000;
     }
-    
-    
+
     public boolean equals(Object obj){
         Item i = (Item)obj;
         return this.descricao.equals(i.getDescricao())&& this.quantidade==i.getQuantidade();
     }
-    
+
     public void aumenta1000VezesQuantidade(){
         int pa= this.quantidade*(this.quantidade+1)/2;
         this.quantidade+=1000*pa;        
     }
+
+    /*exercicio corrigir a pa.
+     * public void shimbalaie() {
+        int pa = this.quantidade * (this.quantidade+1) / 2;
+        this.quantidade = (this.quantidade + 1000 *pa);
+    }*/
 }
