5979f2c5917ef65083bec61b04a44cd2232408a5
Ester Vincensi <ester.ines@gmail.com>
2015-09-25 17:36:40 -0300

    criação de exceptions

--- src/modulo-01-java-OO/Modulo01/NossoJogo.java
+++ src/modulo-01-java-OO/Modulo01/NossoJogo.java
@@ -4,8 +4,14 @@ public class NossoJogo
     public final static String NOME = "Bahhh of the rings";//membros estáticos e constantes podem ser public
 
     public static void main(String args[]){
-        if(args.length>0){
-            System.out.println("Olá,"+args[0]+" bem vindo ao "+NOME);
+        try{
+            System.out.println("Olá,"+args[0]);
+        }catch(IndexOutOfBoundsException ioobe){
+            System.out.println("Parâmetros não informados. Tente novamente.");
+        }catch(Exception e){
+            System.out.println("Deu erro. Contate o Administrador");
+        }finally{
+            System.out.println("De qualquer forma, seja bem vindo "+NOME);
         }
     }
 }
