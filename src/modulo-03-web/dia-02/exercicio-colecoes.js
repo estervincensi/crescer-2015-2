0761a223d379118dcb04d7409fe72fb32c7e7455
Ester Vincensi <ester.ines@gmail.com>
2015-10-20 15:20:24 -0200

    Mais correções dos exercícios da lista 02

--- src/modulo-03-web/dia-02/exercicio-colecoes.js
+++ src/modulo-03-web/dia-02/exercicio-colecoes.js
@@ -2,41 +2,50 @@
 
 //Exercicio 01.a
 function ordenaPorNacionais(array){
-  return array.sort(function(a,b){return a.titulos[0].qtd<b.titulos[0].qtd;});
+  //return array.sort(function(a,b){return a.titulos[0].qtd<b.titulos[0].qtd;});
+  return ordena(0,array);
 };
 
 //Exercicio 01.b
 function ordenaPorContinental(array){
-  return array.sort(function(a,b){return a.titulos[1].qtd<b.titulos[1].qtd;});
+  //return array.sort(function(a,b){return a.titulos[1].qtd<b.titulos[1].qtd;});
+  return ordena(1,array);
 };
 
 //Exercicio 01.c
 function ordenaPorMundial(array){
-  return array.sort(function(a,b){return a.titulos[2].qtd<b.titulos[2].qtd;});
+  //return array.sort(function(a,b){return a.titulos[2].qtd<b.titulos[2].qtd;});
+  return ordena(2,array);
 };
+
+function ordena(tipo,array){
+   return array.sort(function(a,b){return a.titulos[tipo].qtd<b.titulos[tipo].qtd;});
+}
 
 //Exercicio 02
 
 //Exercicio 02.a
 function somarPorNacionais(array){
-  var soma=0;
-  array.forEach(function(elem){soma+=elem.titulos[0].qtd;});
-  return soma;
+  return soma(0,array);
 };
 
 //Exercicio 02.b
 function somarPorContinentais(array){
-  var soma=0;
-  array.forEach(function(elem){soma+=elem.titulos[1].qtd;});
-  return soma;
+  return soma(1,array);
 };
 
 //Exercicio 02.c
-function somarPorMundiais(array){
-  var soma=0;
-  array.forEach(function(elem){soma+=elem.titulos[2].qtd;});
-  return soma;
+function somarPorTodosTitulos(array){
+  var somaTudo =0;
+  somaTudo = somarPorNacionais(array)+somarPorContinentais(array)+soma(2,array);
+  return somaTudo;
 };
+
+function soma(tipo,array){
+  var soma=0;
+  array.forEach(function(elem){soma+=elem.titulos[tipo].qtd;});
+  return soma;
+}
 
 //Exercicio 03
 function apenasOsMelhores(array){
