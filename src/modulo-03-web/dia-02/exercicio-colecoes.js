9b70492912a3839e1acee7a27bfe93af960fb67d
Ester Vincensi <ester.ines@gmail.com>
2015-10-20 13:35:29 -0200

    Exercicios lista 02 - coleções.js

---
+++ src/modulo-03-web/dia-02/exercicio-colecoes.js
@@ -0,0 +1,46 @@
+//Exercicio 01
+
+//Exercicio 01.a
+function ordenaPorNacionais(array){
+  array.sort(function(a,b){return a.titulos[0].qtd<b.titulos[0].qtd;});
+};
+
+//Exercicio 01.b
+function ordenaPorContinental(array){
+  array.sort(function(a,b){return a.titulos[1].qtd<b.titulos[1].qtd;});
+};
+
+//Exercicio 01.c
+function ordenaPorMundial(array){
+  array.sort(function(a,b){return a.titulos[2].qtd<b.titulos[2].qtd;});
+};
+
+//Exercicio 02
+
+//Exercicio 02.a
+function somarPorNacionais(array){
+  var soma=0;
+  array.forEach(function(elem){soma+=elem.titulos[0].qtd;});
+  return soma;
+};
+
+//Exercicio 02.b
+function somarPorContinentais(array){
+  var soma=0;
+  array.forEach(function(elem){soma+=elem.titulos[1].qtd;});
+  return soma;
+};
+
+//Exercicio 02.c
+function somarPorMundiais(array){
+  var soma=0;
+  array.forEach(function(elem){soma+=elem.titulos[2].qtd;});
+  return soma;
+};
+
+//Exercicio 03
+function apenasOsMelhores(array){
+  return array.filter(function(elem){
+    return elem.titulos[0].qtd>=18;
+  })
+};
