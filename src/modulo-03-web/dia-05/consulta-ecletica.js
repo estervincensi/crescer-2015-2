ff017b3c0cded08d9a095381f819bb8bd734cb39
Ester Vincensi <ester.ines@gmail.com>
2015-10-23 10:17:32 -0200

    Exercício 02 - consulta eclética alterações no exercício 1 - alguns ";" que estavam faltando

---
+++ src/modulo-03-web/dia-05/consulta-ecletica.js
@@ -0,0 +1,33 @@
+function buscarArtista (){
+  var artista = $('#artista').val().split(" ").join("+");
+  $.get('https://api.spotify.com/v1/search?query='+artista.replace(' ','+')+'&type=artist').done(function(data){
+    if(data.artists.items.length){
+      consultarAlbuns(data.artists.items[0].id);
+    }else{
+      alert(artista+' não encontrado!');
+    }
+  });
+};
+
+$('#pesquisa').click(buscarArtista);
+$.ajaxSetup({
+  error: function(xhr, status, error) {
+    alert('Caro usuário, devido a um erro '+xhr.status+', não foi possível pesquisar por '+$('#artista').val());
+  }
+});
+
+function consultarAlbuns(id){
+  $('ul:eq(0)').empty();
+  $.get('https://api.spotify.com/v1/artists/'+id+'/albums?limit=50')
+  .done(function(data) {
+    var albuns = data.items;
+    albuns.forEach(function(elem) {
+      elem.images.length &&
+      $('.albuns').append(
+        $('<li>')
+        .append($('<img>')
+        .attr('src', elem.images[1].url))
+      )
+    });
+  });
+}
