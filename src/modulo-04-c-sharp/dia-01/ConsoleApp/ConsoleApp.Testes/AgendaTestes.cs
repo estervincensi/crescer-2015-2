2d1dc69c9583fa9f6dd849cc64ed668d30bf8b77
Ester Vincensi <ester.ines@gmail.com>
2015-10-27 13:34:40 -0200

    Alterações nos exercicios - criação de testes e correção do que não estava funcionando

--- src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp.Testes/AgendaTestes.cs
+++ src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp.Testes/AgendaTestes.cs
@@ -13,5 +13,51 @@ namespace ConsoleApp.Testes
             agenda.AdicionarContato(new Contato() {Nome = "Julio Cesar", Numero = 12345543 });
             Assert.AreEqual(agenda.QuantidadeContatos, 1);
         }
+
+        [TestMethod]
+        public void AgendaRemoveContatoPorNome()
+        {
+            var agenda = new Agenda();
+            agenda.AdicionarContato(new Contato() { Nome = "Ester", Numero = 12345543 });
+            agenda.AdicionarContato(new Contato() { Nome = "Teste", Numero = 123456789 });
+
+            agenda.RemoverContatoPorNome("Ester");
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void AgendaRemoveContatoPorNumero()
+        {
+            var agenda = new Agenda();
+            agenda.AdicionarContato(new Contato() { Nome = "Ester", Numero = 12345543 });
+            agenda.AdicionarContato(new Contato() { Nome = "Teste", Numero = 123456789 });
+
+            agenda.RemoverContatoPorNumero(12345543);
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void AgendaRemoveMaisDeUmContatoPorNumeroSeTiverMesmoNumero()
+        {
+            var agenda = new Agenda();
+            agenda.AdicionarContato(new Contato() { Nome = "Ester", Numero = 12345543 });
+            agenda.AdicionarContato(new Contato() { Nome = "Abdefg", Numero = 12345543 });
+            agenda.AdicionarContato(new Contato() { Nome = "Teste", Numero = 123456789 });
+
+            agenda.RemoverContatoPorNumero(12345543);
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void AgendaRemoveMaisDeUmContatoPorNomeSeTiverMesmoNome()
+        {
+            var agenda = new Agenda();
+            agenda.AdicionarContato(new Contato() { Nome = "Ester", Numero = 12345543 });
+            agenda.AdicionarContato(new Contato() { Nome = "Ester", Numero = 76543274 });
+            agenda.AdicionarContato(new Contato() { Nome = "Teste", Numero = 123456789 });
+
+            agenda.RemoverContatoPorNome("Ester");
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
     }
 }
--- src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp/Program.cs
+++ src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp/Program.cs
@@ -11,9 +11,10 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             String opcao;
+            var agenda = new Agenda();
             do
             {
-                var agenda = new Agenda();
+                Console.Clear();
                 Console.WriteLine("1 - Inserir contato");
                 Console.WriteLine("2 - Excluir contato por nome");
                 Console.WriteLine("3 - Excluir contato por numero");
@@ -35,6 +36,7 @@ namespace ConsoleApp
                     contato.Numero = Convert.ToInt32(numero);
 
                     agenda.AdicionarContato(contato);
+
                 }
                 else if (opcao == "2")
                 {
@@ -52,17 +54,17 @@ namespace ConsoleApp
                 }
                 else if (opcao == "4")
                 {
+                    Console.WriteLine("Lista de Contatos");
                     List<Contato> lista = agenda.ListarContatos();
                     foreach (var contato in lista)
                     {
-                        Console.WriteLine(contato);
+                        Console.WriteLine("Nome:"+contato.Nome+"\nNumero: "+contato.Numero);
                     }
+                    Console.WriteLine("Digite uma tecla para continuar: ");
+                    Console.ReadLine(); ;   
                 }
 
             } while (opcao != "5");
-
-
-            Console.ReadLine();
         }
     }
 }
