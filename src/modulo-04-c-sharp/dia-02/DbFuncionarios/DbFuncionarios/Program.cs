878eac43e66ec02c235e425796704820816ca4d2
Ester Vincensi <ester.ines@gmail.com>
2015-10-27 23:46:01 -0200

    Exercicios a,b,c,e,f,g. Ainda não fiz os testes.

--- src/modulo-04-c-sharp/dia-02/DbFuncionarios/DbFuncionarios/Program.cs
+++ src/modulo-04-c-sharp/dia-02/DbFuncionarios/DbFuncionarios/Program.cs
@@ -10,146 +10,98 @@ namespace DbFuncionarios
     {
         static void Main(string[] args)
         {
-            //dynamic[] projecao = BuscarNomeEIdETituloDoCargoDeFuncionarios();
+            Console.WriteLine("Ordenados por cargo");
+            IList<Funcionario> lista = OrdenadosPorCargo();
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item.Nome + " - " + item.Cargo.Titulo + "\n");
+            }
 
-            //foreach (dynamic item in projecao)
-            //{
-            //    Console.WriteLine(item.Id);
-            //    Console.WriteLine(item.Nome);
-            //    Console.WriteLine(item.TituloCargo);
-            //    Console.WriteLine();
-            //}
-            
+            Console.WriteLine("funcionarios por turno:");
+            IList<dynamic> lista1 = QtdFuncionariosPorTurno();
+            foreach (var item in lista1)
+            {
+                Console.WriteLine(item.turno + " - " + item.count);
+            }
             Console.Read();
         }
 
-        static void Criarasdasd(int? id)
+        static IList<Funcionario> OrdenadosPorCargo()
         {
-            if(id.HasValue)
-            {
-                Console.WriteLine("Tem valor");
-            }
-            else
-            {
-                Console.WriteLine(id.Value);
-            }
+            var baseDeDados = new BaseDeDados();
+            List<Funcionario> funcionarios = baseDeDados.Funcionarios;
+
+            return funcionarios.OrderBy(f => f.Cargo.Titulo).ToList();
+
         }
 
-        static dynamic[] BuscarNomeEIdETituloDoCargoDeFuncionarios()
+        static IList<Funcionario> BuscarPorNome(string nome)
         {
             var baseDeDados = new BaseDeDados();
             List<Funcionario> funcionarios = baseDeDados.Funcionarios;
 
             var query = from f in funcionarios
+                        where f.Nome.Contains(nome)
+                        select f;
+            return query.OrderBy(f => f.Nome).ToList();
+            //return funcionarios.Where(f => f.Nome.Contains(nome)).OrderBy(f=>f.Nome).ToList(); 
+
+        }
+
+        static IList<dynamic> BuscaRapida(string nome)
+        {
+            var baseDeDados = new BaseDeDados();
+            List<Funcionario> funcionarios = baseDeDados.Funcionarios;
+
+            var query = from f in funcionarios
+                        where f.Nome.Contains(nome)
                         select new
                         {
-                            Id = f.Id,
                             Nome = f.Nome,
                             TituloCargo = f.Cargo.Titulo
                         };
 
-            return query.ToArray();
+            return query.ToList<dynamic>();
         }
 
-        static IList<Funcionario> BuscarOrdenadosPorNome()
+        //static IList<dynamic> BuscaRapida(string nome)
+        //{
+        //    var baseDeDados = new BaseDeDados();
+        //    return baseDeDados.Funcionarios
+        //      .Where(x => x.Nome.Contains(nome))
+        //     .Select(x => new { Nome = x.Nome, TituloCargo = x.Cargo.Titulo })
+        //     .ToList<dynamic>();
+        //}
+
+
+        static IList<dynamic> QtdFuncionariosPorTurno()
         {
             var baseDeDados = new BaseDeDados();
             List<Funcionario> funcionarios = baseDeDados.Funcionarios;
 
-            var resultado = funcionarios.OrderByDescending(funcionario => funcionario.Nome).ToList();
-
-            return resultado;
-        }
-
-        static Funcionario BuscarPorId(int id)
-        {
-            var baseDeDados = new BaseDeDados();
-            List<Funcionario> funcionarios = baseDeDados.Funcionarios;
-            //
-            //return funcionarios.FirstOrDefault(funcionario => funcionario.Id == id);
-
-            IEnumerable<Funcionario> query = from funcionario in funcionarios
-                                             where funcionario.Id == id
-                                             select funcionario;
-            
-            //query = funcionarios.Where(f => f.Id == id).FirstOrDefault();
-
-            return query.FirstOrDefault();
-        }
-
-        static IList<Funcionario> BuscarPorCargo(string tituloCargo)
-        {
-            var baseDeDados = new BaseDeDados();
-            List<Funcionario> funcionarios = baseDeDados.Funcionarios;
-
-            return funcionarios.Where(f => CompararIgnoreCase(f.Cargo.Titulo, tituloCargo)).ToList();
-        }
-
-        static bool CompararIgnoreCase(string a, string b)
-        {
-            if (!String.IsNullOrEmpty(a) && !String.IsNullOrEmpty(b))
+            return funcionarios.GroupBy(f => f.TurnoTrabalho).Select(f => new
             {
-                return a.ToUpper() == b.ToUpper();
-            }
+                turno = f.Key,
+                count = f.Count()
+            }).ToList<dynamic>();
 
-            return a == b;
+        }
+
+        static IList<Funcionario> BuscarPorCargo(Cargo cargo)
+        {
+            var baseDeDados = new BaseDeDados();
+            List<Funcionario> funcionarios = baseDeDados.Funcionarios;
+
+            return funcionarios.Where(f => f.Cargo.Equals(cargo)).ToList();
+        }
+
+        static IList<Funcionario> FiltrarPorIdadeAproximada(int idade)
+        {
+            var baseDeDados = new BaseDeDados();
+            List<Funcionario> funcionarios = baseDeDados.Funcionarios;
+            return funcionarios.Where(f => f.DataNascimento.AddYears(idade).Equals(DateTime.Now.Year) || f.DataNascimento.AddYears(idade - 5).Equals(DateTime.Now.Year) || f.DataNascimento.AddYears(idade + 5).Equals(DateTime.Now.Year)).ToList();
+
         }
 
     }
 }
-
-
-
-//short, int, long
-//Int16, Int32, Int64
-//ushort, uint, ulong
-//float, double, decimal
-//String
-//List<int> lista = new List<int>();
-//int[] arrai = new int[6];
-//var lista = new List<int>();
-//var diasDaSemana = new Dictionary<int, Dictionary<string, string>>();
-
-//var pessoa = new
-//{
-//    Id = 1,
-//    Nome = "Ben-hur"
-//};
-
-//dynamic obj = new ExpandoObject();
-//obj.Id = 1;
-//obj.Nome = "asdasd";
-
-//DateTime data = new DateTime(2015, 7, 25);
-//DateTime.Now
-
-//DateTime dt = null;
-
-//var diff = ( DateTime.Now - DateTime.Now.AddDays(-1) );
-
-
-
-
-
-
-
-//long[] arrai = { 1, 2, 3, 4, 3, 10, 3 };
-
-//bool temNumero3 = false;
-
-//temNumero3 = arrai.Any(item => item == 3);
-
-//long numero3 = arrai.FirstOrDefault(item => item == 8);
-
-//var numerosPares = arrai.Count(numero => numero % 2 == 0 && numero > 4);
-
-//for (int i = 0; i < arrai.Length; i++)
-//{
-//    if(arrai[i] == 3)
-//    {
-//        temNumero3 = true;
-//        break;
-//    }
-//}
-
-//Console.WriteLine(temNumero3);
