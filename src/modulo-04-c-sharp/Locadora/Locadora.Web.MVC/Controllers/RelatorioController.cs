73cb938d6ec2ecd4bc561e16c0755f95e06d23ae
Ester Vincensi <ester.ines@gmail.com>
2015-11-04 17:35:10 -0200

    Exercício Locadora feito em aula.

--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
@@ -1,4 +1,5 @@
-﻿using Locadora.Web.MVC.Models;
+﻿using Locadora.Dominio.Repositorio;
+using Locadora.Web.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,32 @@ namespace Locadora.Web.MVC.Controllers
 {
     public class RelatorioController : Controller
     {
+        private IJogoRepositorio repositorio = new Repositorio.ADO.JogoRepositorio();
         public ActionResult JogosDisponiveis()
         {
-            var model = new List<JogoModel>()
-            {
-                new JogoModel() { Id = 1, Nome = "Teste", Preco = 9.9m, Categoria="RPG" },
-                new JogoModel() { Id = 1, Nome = "dasd", Preco = 5m, Categoria="Aventura" },
-                new JogoModel() { Id = 1, Nome = "dasda", Preco = 10m, Categoria="RPG" }
-            };
 
+            var model = new RelatorioModel();
+            decimal valorJogoMaisCaro = 0;
+            decimal valorJogoMaisBarato = 10000000000;
+            decimal valorTotal = 0;
+            foreach(var jogo in repositorio.BuscarTodos())
+            {
+                var jogoModel = new JogoModel() { Nome = jogo.Nome, Preco = jogo.Preco, Categoria = jogo.Categoria.ToString()};
+                model.Jogos.Add(jogoModel);
+                if (jogo.Preco > valorJogoMaisCaro)
+                {
+                    valorJogoMaisCaro = jogo.Preco;
+                    model.JogoMaisCaro = jogo.Nome;
+                }
+                if(jogo.Preco < valorJogoMaisBarato)
+                {
+                    valorJogoMaisBarato = jogo.Preco;
+                    model.JogoMaisBarato = jogo.Nome;
+                }
+                valorTotal += jogo.Preco;
+                model.QuantidadeTotalDeJogos++;
+            }
+            model.ValorMedio = valorTotal/ model.QuantidadeTotalDeJogos;
             return View(model);
         }
     }
